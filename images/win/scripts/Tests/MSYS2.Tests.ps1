79373470019fc9ecf1a55e6c7c13673f7770f247
VyacheslavV <v.zabayrachny@akvelon.com>
2021-06-01 12:19:15 +0300

    add test install packege

--- images/win/scripts/Tests/MSYS2.Tests.ps1
+++ images/win/scripts/Tests/MSYS2.Tests.ps1
@@ -1,23 +1,59 @@
-BeforeAll {
-    $msys2BinDir = "C:\msys64\usr\bin"
-    $msys2mingwDir = "C:\msys64\mingw64\bin"
+function Test-VersionOutput {
+    param (
+        [Parameter(Mandatory)] [string] $Executable,
+        [string] $CallParameter = "version", 
+        [string] $Dash
+    )
+    try {
+        $ChangeParam = $dash + $CallParameter
+        $fullCommand = "$Executable $ChangeParam"
+        $result = ShouldReturnZeroExitCode -ActualValue $fullCommand
+        if (-not $result.Succeeded)
+        {
+            throw
+        }
+        Write-Host "Tool '$Executable' installed"
+    }
+    catch {
+        if ( $Dash.Length -eq 2  )
+        {
+            Write-Host "Tool '$Executable' not installed" 
+            Exit 1
+        }
+        $Dash = $Dash + '-'
+        Test-VersionOutput -Executable $Executable -Dash $Dash 
+    }
+    return
 }
 
-Describe "MSYS2" {
-    It "<ToolName>" -TestCases @(
-        @{ ToolName = "bash.exe" }
-        @{ ToolName = "tar.exe" }
-        @{ ToolName = "make.exe" }
-    ) {
-        Join-Path $msys2BinDir $ToolName | Should -Exist
+$archs = (Get-ToolsetContent).MsysPackages.mingw.arch
+ foreach ($arch in $archs)
+ {
+    if ($arch -eq "mingw-w64-x86_64")
+    {
+        $env:PATH = "C:\msys64\mingw64\bin;C:\msys32usr\bin;$origPath"
+    }
+    else
+    {
+        $env:PATH = "C:\msys64\mingw32\bin;C:\msys32usr\bin;$origPath"
     }
 
-    It "<ToolName>" -TestCases @(
-        @{ ToolName = "gcc.exe" }
-        @{ ToolName = "cmake.exe" }
-        @{ ToolName = "g++.exe" }
-        @{ ToolName = "clang-tidy.exe" }
-    ) {
-        Join-Path $msys2mingwDir $ToolName | Should -Exist
+    $tools = (Get-ToolsetContent).MsysPackages.mingw.arch.name
+
+    foreach ( $tool in $tools)
+    { 
+         $Executables = (Get-ToolsetContent).MsysPackages.mingw.arch.name.executable
+
+         foreach ( $Executable in $Executables )  
+
+            $Executables = (Get-ToolsetContent).MsysPackages.mingw.arch.name.
+
+
+            Describe "$Executable" {
+            It "$Executable" -Testcases @{Executable=$Executable} {
+                Test-VersionOutput -Executable $Executable | Should -ReturnZeroExitCode
+            }
+
+            }
     }
 }
