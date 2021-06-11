e2df869342e1b09f7f8aa576cb189dab4096cc51
VyacheslavV <v.zabayrachny@akvelon.com>
2021-06-11 20:42:01 +0300

    add test dir

--- images/win/scripts/Tests/MSYS2.Tests.ps1
+++ images/win/scripts/Tests/MSYS2.Tests.ps1
@@ -2,43 +2,40 @@ $origPath = $env:PATH
 $toolsetContent = (Get-ToolsetContent).MsysPackages
 $archs = $toolsetContent.mingw.arch
 
-BeforeAll {
-    $msys2Dir = "C:\msys64"
-}
 
 Describe "MSYS2" {
+    Context 'MSYS2' {
+        BeforeAll {
+            $msys2Dir = "C:\msys64"
+            $env:PATH = "C:\msys64\usr\bin;$origPath"
+        }
+        
+        It "msys2Dir" {
+            $msys2Dir | Should -Exist
+        }
 
-    It "msys2Dir" {
-        $msys2Dir | Should -Exist
-    }
+        
+        $TestCases = @(
+            @{ ToolName = "bash.exe" }
+            @{ ToolName = "tar.exe" }
+            @{ ToolName = "make.exe" }
+        )
 
-    $env:PATH = "C:\msys64\usr\bin;C:\msys32usr\bin;$origPath"
-    $TestCases = @(
-        @{ ToolName = "bash.exe" }
-        @{ ToolName = "tar.exe" }
-        @{ ToolName = "make.exe" }
-    )
+        It "call <ToolName>" -TestCases $TestCases {
+            (get-command "$ToolName").Source | Should -BeLike "$msys2Dir*"
+        }
 
-    It "call <ToolName>" -TestCases $TestCases {
-        (get-command "$ToolName").Source | Should -BeLike "$msys2Dir*"
-    }
-
-    It "way <ToolName>" -TestCases $TestCases {
-        "$ToolName" | Should -TestExecutable
+        It "way <ToolName>" -TestCases $TestCases {
+            "$ToolName" | Should -TestExecutable
+        }
     }
 }
+
+
+
 
 foreach ($arch in $archs){
     Describe "$arch" {
-        
-        if ($arch -eq "mingw-w64-i686")
-        {
-            $env:PATH = "C:\msys64\mingw32\bin;C:\msys32usr\bin;$origPath"
-        }
-        else
-        {
-            $env:PATH = "C:\msys64\mingw64\bin;C:\msys32usr\bin;$origPath"
-        }
 
         $archPackages = $toolsetContent.mingw | Where-Object { $_.arch -eq $arch }
         $tools = $archPackages.runtime_packages.name | ForEach-Object { "$_" }
@@ -46,12 +43,30 @@ foreach ($arch in $archs){
         foreach ( $tool in $tools){
 
             Context "$tool"{
+                        
+            BeforeAll {
+                if ($arch -eq "mingw-w64-i686")
+                {
+                    $env:PATH = "C:\msys64\mingw32\bin;$origPath"
+                    $ExecDir = "C:\msys64\mingw32"
+                }
+                else
+                {
+                    $env:PATH = "C:\msys64\mingw64\bin;$origPath"
+                    $ExecDir = "C:\msys64\mingw64"
+                }
+            }
 
             $executables = ($archPackages.runtime_packages | Where-Object { $_.name -eq $tool }).executables | ForEach-Object { @{ExecName = $_} }
 
             It "Executable <ExecName> is installed" -TestCases $executables {
                 "$ExecName" | Should -TestExecutable
             }
+            
+            It "way <ExecName>" -TestCases $executables {
+                (get-command "$ExecName").Source | Should -BeLike "$ExecDir*"
+            }
+
             }
         }
     }
